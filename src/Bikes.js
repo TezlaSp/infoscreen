2dda5d071075cac8d564448d2d18e90b0e35186d
Nicolai Andersen <nicolaihai@hotmail.no>
2021-02-09 13:52:47 +0100

    completed version one of infoscreen

---
+++ src/Bikes.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import createEnturService from '@entur/sdk';
+
+const service = createEnturService({
+    clientName: 'Nicolai-Infoscreen'
+})
+
+function Bikes() {
+    const [bikeStations, setBikeStations] = useState([]);
+
+    useEffect(() => {
+        service.getBikeRentalStationsByPosition(
+            {latitude: 63.428211, longitude: 10.392514},
+            230
+        ).then((data) => setBikeStations(data));
+    }, [])
+
+    return (<div className="bike">
+        <h2>City Bikes</h2>
+        {bikeStations.map((stationData) => (
+        <Station key={stationData.name} data={stationData} />
+        ))}
+    </div>)
+}
+
+export default Bikes;
+
+function Station(props) {
+    const { data } = props;
+    return <div className="station">
+        {data.name}: {data.bikesAvailable} - {data.spacesAvailable};
+        </div>;
+}
